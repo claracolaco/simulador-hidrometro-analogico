411466e681a6a15ed8266baa493b42cf1aceb661
Maria Clara Colaço <colacoclara0@gmail.com>
2025-09-09 16:12:12 -0300

    Update Display.hpp

--- Display.hpp
+++ Display.hpp
@@ -6,19 +6,15 @@
 
 class Display {
 private:
-    std::string formato;   // ex.: "jpeg" (para futuras versões com imagem)
+    std::string formato;   
 
 public:
-    // Construtor
     Display(std::string formato = "texto");
 
-    // Mostra a leitura no terminal
     void desenharMostrador(const Medicao& medicao) const;
 
-    // Adiciona um texto extra sobreposto (ex.: "Simulação em andamento...")
     void sobreporTexto(const std::string& texto) const;
 
-    // Getter
     std::string getFormato() const;
 };
 
