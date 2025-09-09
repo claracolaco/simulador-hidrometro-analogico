0470ab20c9eae1ab17b5b0b2a76a456d1621b1a6
Maria Clara Colaço <colacoclara0@gmail.com>
2025-09-09 16:16:31 -0300

    Update Saida.hpp

--- Saida.hpp
+++ Saida.hpp
@@ -7,21 +7,17 @@
 
 class Saida {
 private:
-    std::string caminhoImagem; // caminho para salvar a saída
-    float taxaImg_m3;          // intervalo em m³ para gerar saída
-
+    std::string caminhoImagem; 
+    float taxaImg_m3;          
 public:
-    // Construtor
     Saida(std::string caminho = "saida.txt", float taxa = 1.0f);
 
-    // Gera a "imagem" (nesta versão: salva em arquivo .txt)
+    // gera a "imagem" (nessa versão, salva em arquivo .txt)
     void gerarJPEG(const Medicao& m, const Display& d) const;
 
-    // Getters
     std::string getCaminhoImagem() const;
     float getTaxaImg_m3() const;
 
-    // Setters
     void setCaminhoImagem(const std::string& caminho);
     void setTaxaImg_m3(float taxa);
 };
