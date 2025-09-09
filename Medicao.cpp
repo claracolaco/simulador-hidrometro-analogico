388f03319fb900b59847c3662525a7ba69200c4e
Maria Clara Colaço <colacoclara0@gmail.com>
2025-09-09 16:15:21 -0300

    Update Medicao.cpp

--- Medicao.cpp
+++ Medicao.cpp
@@ -1,30 +1,24 @@
 #include "Medicao.hpp"
-#include <iomanip>   // para std::setw e std::setfill
-#include <sstream>   // para montar string formatada
+#include <iomanip>   
+#include <sstream>  
 
-// Construtor: inicia volume zerado
 Medicao::Medicao(int limite) {
     volumeTotal_m3 = 0.0;
     mm = 0;
     limiteOdometro = limite;
 }
 
-// Soma volume ao total e atualiza mm + odômetro
 void Medicao::contabilizar(double volume_m3) {
-    // Atualiza volume total
     volumeTotal_m3 += volume_m3;
 
-    // Atualiza mm (subunidades vermelhas)
     mm = static_cast<int>((volumeTotal_m3 - static_cast<int>(volumeTotal_m3)) * 1000);
 
-    // Reseta se ultrapassar limite do odômetro
     if (volumeTotal_m3 >= limiteOdometro) {
         volumeTotal_m3 = 0.0;
         mm = 0;
     }
 }
 
-// Retorna string formatada da leitura (ex.: "01234 m³ | 067 mm")
 std::string Medicao::leituraTexto() const {
     std::ostringstream oss;
 
@@ -36,7 +30,6 @@ std::string Medicao::leituraTexto() const {
     return oss.str();
 }
 
-// Getters
 double Medicao::getVolumeTotal_m3() const {
     return volumeTotal_m3;
 }
